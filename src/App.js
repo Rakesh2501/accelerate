5bcfaca9c1ad74ad3add8f72cd52e57b66ba4711
Rakesh2501 <rakeshprabhu1@gmail.com>
2020-10-23 20:42:35 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,130 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
+import {AppBar, Tabs, Toolbar,Typography,Tab, Link,Drawer, IconButton, Divider, ListItem, ListItemText,List} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import {createMuiTheme,ThemeProvider,makeStyles,withStyles} from '@material-ui/core/styles'
+import { ChevronLeft } from '@material-ui/icons';
+import clsx from 'clsx';
+import Dashboard from './components/Dashboard'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const themes = createMuiTheme({
+  palette:{
+    primary:{
+      main:'#2196f3',
+    },
+    secondary:{
+      main:'#e3f2fd'
+    }
+  }
+})
+
+const styles = (theme) =>({
+  drawerWidth:{
+    width:160
+  },
+  topDivider:{
+    height:63
+  },
+  appBar:{
+    transition:theme.transitions.create(['margin','width'],{easing:theme.transitions.easing.sharp, 
+      // duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift:{
+    width:`calc(100% - 160px)`,
+    marginLeft:160,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      // duration: theme.transitions.duration.enteringScreen,
+    })
+  },
+  content:{
+    marginLeft:180,
+    marginRight:40,
+    marginTop:80,
+    transition:theme.transitions.create(['margin','width'],{
+      easing:theme.transitions.easing.sharp,
+      // duration: theme.transitions.duration.leavingScreen,
+    })
+  },
+  contentShift:{
+    marginLeft:200,
+    width:`100% - 160px`,
+    transition: theme.transitions.create(['margin','width'], {
+      easing: theme.transitions.easing.easeOut,
+      // duration: theme.transitions.duration.enteringScreen,
+    })
+  },
+  paper:{
+    background:'#e3f2fd'
+  }
+})
+
+class App extends React.Component{
+
+  constructor(){
+    super()
+    this.state={
+      drawerOpen:false
+    }
+  }
+
+  handleDrawer=()=>{
+    this.setState({drawerOpen:!this.state.drawerOpen})
+  }
+
+  render(){
+
+    const { classes } = this.props;
+
+    return(
+      <React.Fragment>
+        <ThemeProvider theme={themes}>
+          <Router>
+            <AppBar position="fixed" color="primary" className={clsx(classes.appBar,{[classes.appBarShift]:this.state.drawerOpen})}>
+              <Toolbar>
+                <IconButton color="inherit" onClick={this.handleDrawer} edge="start">
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h5">Vanguard</Typography>
+                <Tabs>
+                  <Tab label="Participant" />
+                </Tabs>
+              </Toolbar>
+            </AppBar>
+
+            <Drawer variant="persistent" anchor="left" open={this.state.drawerOpen} classes={{paper:classes.paper}}>
+              <div className={clsx(classes.drawerWidth, classes.topDivider)} style={{ display: 'flex',flexDirection:'row',justifyContent:'flex-end'}}>
+                
+                  <IconButton onClick={this.handleDrawer}>
+                    <ChevronLeft />
+                  </IconButton>
+                
+              </div>
+                <Divider/>
+                <List>
+                  <ListItem button>
+                    <ListItemText>Dashboard</ListItemText>
+                  </ListItem>
+                  <ListItem button>
+                    <ListItemText>Accounts</ListItemText>
+                  </ListItem>
+                  <ListItem button>
+                    <ListItemText>Activity</ListItemText>
+                  </ListItem>
+                </List>
+            </Drawer>
+
+            <Route path='/' render={()=><Dashboard classes={classes} theme = {themes} drawerOpen={this.state.drawerOpen}/>} />
+            
+          </Router>
+        </ThemeProvider>
+      </React.Fragment>
+    )
+  }
+
 }
 
-export default App;
+export default withStyles(styles)(App);
+// export default App;
---
+++ src/components/Dashboard.js
@@ -0,0 +1,160 @@
+import { Grid, Typography,Paper, Card } from '@material-ui/core';
+import clsx from 'clsx';
+import React from 'react';
+import {Doughnut} from  'react-chartjs-2'
+import {withStyles} from '@material-ui/core/styles'
+
+
+const state = {
+    labels: ['Health','Misc Health','Other','Retirement'],
+    datasets: [
+      {
+        label: 'Rainfall',
+        backgroundColor: [
+          '#B21F00',
+          '#C9DE00',
+          '#2FDE00',
+          '#00A6B4',
+          
+        ],
+        hoverBackgroundColor: [
+        '#501800',
+        '#4B5000',
+        '#175000',
+        '#003350',
+        '#35014F'
+        ],
+        data: [65, 59, 80, 81]
+      }
+    ]
+  }
+
+const styles=(theme)=>({
+
+    paper:{
+        // padding:15,
+        width:'80%',
+        height:300,
+        backgroundColor:'#e3f2fd',
+        padding:theme.spacing(3)
+    },
+    doughnut:{
+        width:'80%',
+        height:'70%',
+        paddingTop:25
+    }
+
+})
+
+
+class Dashboard extends React.Component{
+    constructor(props){
+        super(props)
+
+    }
+
+    render(){
+
+        const {classes,themes} = this.props
+
+        return(
+            <div style={{ display: 'flex', flexDirection: 'row' }} className={clsx(classes.content, { [classes.contentShift]: this.props.drawerOpen })}>
+                <Grid container justify="space-around" spacing={3}>
+                    <Grid item lg={12} md={12} sm={12} xs = {12}>
+                        <Typography>Welcome to your personalized benefit dashboard, Anna</Typography>
+                    </Grid>
+                    <Grid item lg={12} md={12} sm={12} xs={12}>
+                        <Typography variant="h3">Dashboard</Typography>
+                    </Grid>
+                    <Grid item xl={6} lg={6} md={6} sm={12} xs={12} spacing={2}>
+                        <Paper className={classes.paper} >
+                            <div style={{ display: 'flex', flexDirection: 'row', marginBottom: 20 }}>
+                                <div style={{ flexGrow: 9 }}>
+                                    <Typography variant="h5">Benefits Overview</Typography>
+                                </div>
+                                <div style={{ flexGrow: 1, paddingTop: 10 }}>
+                                    <Typography style={{ color: 'blue' }}>Edit</Typography>
+                                </div>
+                            </div>
+                            <div className={clsx(classes.doughnut)}>
+                                <Doughnut
+                                    data={state}
+                                    options={{
+                                        legend: {
+                                            display: true,
+                                            position: 'right'
+                                        }
+                                    }}
+                                />
+                            </div>
+
+                        </Paper>
+                    </Grid>
+                    <Grid item lg={6} md={6} sm={12} xs={12}>
+                        <Paper className={classes.paper}>
+                            <div style={{ display: 'flex', flexDirection: 'row', marginBottom: 20 }}>
+                                <div style={{ flexGrow: 9 }}>
+                                    <Typography variant="h5">Contributions Overview</Typography>
+                                </div>
+                                <div style={{ flexGrow: 1, paddingTop: 10 }}>
+                                    <Typography style={{ color: 'blue' }}>Edit</Typography>
+                                </div>
+                            </div>
+                            
+                            <div style={{ display: 'flex', flexDirection: 'column', marginBottom:5 }}>
+                                <div style={{ fontSize: 13 }}>
+                                    <p>TOTAL BALANCE</p>
+                                </div>
+                                <div style={{ marginTop: -20 }}>
+                                    <h4>$0</h4>
+                                </div>
+                            </div>
+
+                            <div style={{ display: 'flex', flexDirection: 'row', marginBottom: 5 }}>
+                                <div style={{ display: 'flex', flexDirection: 'column', marginRight: 60 }}>
+                                    <div style={{ fontSize: 13 }}>
+                                        <p>TOTAL PRUDENTIAL ASSETS</p>
+                                    </div>
+                                    <div style={{ marginTop: -20 }}>
+                                        <h4>$0</h4>
+                                    </div>
+                                </div>
+                                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                    <div style={{ fontSize: 13 }}>
+                                        <p>CONNECTED ACCOUNT BALANCE</p>
+                                    </div>
+                                    <div style={{ marginTop: -20 }}>
+                                        <h4>$0</h4>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                                <div style={{ display: 'flex', flexDirection: 'column', marginRight: 45 }}>
+                                    <div style={{ fontSize: 13 }}>
+                                        <p>TOTAL YEARLY CONSUMPTION</p>
+                                    </div>
+                                    <div style={{ marginTop: -20 }}>
+                                        <h4>$0</h4>
+                                    </div>
+                                </div>
+                                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                    <div style={{ fontSize: 13 }}>
+                                        <p>RISK ATTITUDE</p>
+                                    </div>
+                                    <div style={{ marginTop: -20 }}>
+                                        <h4>MODERATE</h4>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+
+export default withStyles(styles)(Dashboard)
